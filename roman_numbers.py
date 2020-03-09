9286d37e1c4b09a5e850d2927e6d4ec23939f759
manolo2829 <61692427+manolo2829@users.noreply.github.com>
2020-03-09 09:08:19 -0300

    Add files via upload

---
+++ roman_numbers.py
@@ -0,0 +1,14 @@
+def roman_to_decimal(roman_numbers):
+	if roman_numbers == ('I'):
+		return 1
+	if roman_numbers == ('II'):
+		return 2
+	if roman_numbers == ('III'):
+		return 3
+	if roman_numbers == ('IV'):
+		return 4
+	if roman_numbers == ('V'):
+		return 5
+	
+	
+	
